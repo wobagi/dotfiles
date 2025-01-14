358ea719fb72d1cee91269b09edbdb7c31f2ba3d
WB <wobagi@gmail.com>
2025-01-14 01:37:42 +0100

    Add local nameservers

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -18,7 +18,8 @@
 
 
   networking.hostName = "tx"; # Define your hostname.
-  # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
+  networking.nameservers = [ "83.175.144.9" "83.175.144.14" ];
+# networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
 
   # Configure network proxy if necessary
   # networking.proxy.default = "http://user:password@proxy:port/";
