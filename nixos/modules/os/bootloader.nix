9591ba66b923d90a9e013042dff160be7eb19d74
WB <wobagi@gmail.com>
2025-01-27 00:54:27 +0100

    commented out systemd limit

--- nixos/modules/os/bootloader.nix
+++ nixos/modules/os/bootloader.nix
@@ -2,7 +2,7 @@
 {
   boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
-  boot.loader.systemd-boot-configurationLimit = 10;
-  boot.kernelPackages = pkgs.linuxPackages_latest;
+  # boot.loader.systemd-boot-configurationLimit = 10;
+  # boot.kernelPackages = pkgs.linuxPackages_latest;
   boot.kernelParams = [ "acpi.ec_no_wakeup=1" ];
 }
