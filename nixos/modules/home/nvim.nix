24f4ed7d3c1c8720252fd0a04282b1d1ebfa2bbc
WB <wobagi@gmail.com>
2025-01-28 02:08:23 +0100

    nvim setup nvf

--- nixos/modules/home/nvim.nix
+++ nixos/modules/home/nvim.nix
@@ -5,4 +5,15 @@
     enable = true;
     vimAlias = true;
   };
+
+  programs.nvf = {
+    enable = true;
+    settings = {
+      vim.viAlias = false;
+      vim.vimAlias = true;
+      vim.lsp = {
+        enable = true;
+      };
+    };
+  };
 }
