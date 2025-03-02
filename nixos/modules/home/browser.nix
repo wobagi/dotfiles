0a56e43cd12bea8eb9435d5be09ecc3e33c91bbf
WB <wobagi@gmail.com>
2025-03-02 15:28:13 +0100

    try installing vimium in librewolf

--- nixos/modules/home/browser.nix
+++ nixos/modules/home/browser.nix
@@ -1,5 +1,31 @@
 { pkgs, lib, ... }:
 {
-  programs.firefox.enable = true;
+  # programs.firefox.enable = true;
   programs.vivaldi.enable = true;
+  programs.librewolf.enable = true;
+  programs.firefox = {
+    enable = true;
+    package = pkgs.librewolf;
+    policies = {
+      DisableTelemetry = true;
+      DisableFirefoxStudies = true;
+      Preferences = {
+        "cookiebanners.service.mode.privateBrowsing" = 2; # Block cookie banners in private browsing
+        "cookiebanners.service.mode" = 2; # Block cookie banners
+        "privacy.donottrackheader.enabled" = true;
+        "privacy.fingerprintingProtection" = true;
+        "privacy.resistFingerprinting" = true;
+        "privacy.trackingprotection.emailtracking.enabled" = true;
+        "privacy.trackingprotection.enabled" = true;
+        "privacy.trackingprotection.fingerprinting.enabled" = true;
+        "privacy.trackingprotection.socialtracking.enabled" = true;
+      };
+      ExtensionSettings = {
+        "vimium@philc" = {
+          install_url = "https://addons.mozilla.org/firefox/downloads/latest/vimium-ff/latest.xpi";
+          installation_mode = "force_installed";
+        };
+      };
+    };
+  };
 }
