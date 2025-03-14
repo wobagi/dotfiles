f1dbaa128359320f38f90a59b65e1aff99d72c65
WB <wobagi@gmail.com>
2025-03-15 00:28:12 +0100

    fix virtualisation typos

--- nixos/modules/os/virtualisation.nix
+++ nixos/modules/os/virtualisation.nix
@@ -1,8 +1,8 @@
 { pkgs, ... }:
 {
   virtualisation.docker.enable = true;
-  virtualisation.docker.rootless.enable = {
+  virtualisation.docker.rootless = {
     enable = true;
     setSocketVariable = true;
-  }
+  };
 }
