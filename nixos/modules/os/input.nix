396a722b2985d4914f6895ecc9d99f8432a0766a
WB <wobagi@gmail.com>
2025-04-06 13:45:29 +0200

    add comment

--- nixos/modules/os/input.nix
+++ nixos/modules/os/input.nix
@@ -1,27 +1,35 @@
 { config, lib, pkgs, ... }:
-
 {
   i18n.inputMethod = {
     enabled = "fcitx5";
-    fcitx5.addons = [ pkgs.fcitx5-mozc ];
+    fcitx5.addons = [ pkgs.fcitx5-mozc ]; # or fcitx5-anthy
   };
 
- # fonts.packages = with pkgs; [
- #   corefonts
- #   noto-fonts
- #   noto-fonts-cjk
- #   noto-fonts-emoji
- #   noto-fonts-extra
- #   nerdfonts
- #   ipafont
- # ];
- # fonts.fontconfig.defaultFonts = {
- #   monospace = [
- #     "Hack Nerd Font"
- #     "Noto Sans Mono CJK JP"
- #   ];
- #   sansSerif = [
- #     "Noto Sans"
- #     "Noto Sans CJK JP"
- #   ];
+  fonts.packages = with pkgs; [
+    corefonts
+    noto-fonts
+    noto-fonts-cjk
+    noto-fonts-emoji
+    noto-fonts-extra
+    nerdfonts
+    ipafont
+    ];
+  fonts.fontconfig.defaultFonts = {
+    monospace = [
+      "Hack Nerd Font"
+      "Noto Sans Mono CJK JP"
+      "DejaVu Sans Mono"
+      "IPAGothic"
+      ];
+    sansSerif = [
+      "Noto Sans"
+      "Noto Sans CJK JP"
+      "DejaVu Sans"
+      "IPAGothic"
+      ];
+    serif = [
+      "DejaVu Serif"
+      "IPAPMincho"
+      ];
+  };
 }
